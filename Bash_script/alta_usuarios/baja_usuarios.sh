41b59e11494d4a3d6266e33540c194646b906795
goohan3242 <goohan3242@gmail.com>
2025-11-20 19:46:31 -0300

    ADD: [baja_usuarios] Agrego baja de usuarios

---
+++ Bash_script/alta_usuarios/baja_usuarios.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+LISTA=$1
+
+ANT_IFS=$IFS
+IFS=$'\n'
+for LINEA in `cat $LISTA |  grep -v ^#`
+do
+	USUARIO=$(echo  $LINEA |awk -F ',' '{print $1}')
+	GRUPO=$(echo  $LINEA |awk -F ',' '{print $2}')
+	DIR_HOME_GRUPO=$(echo  $LINEA |awk -F ',' '{print $3}')
+	sudo userdel -r $USUARIO 
+done
+IFS=$ANT_IFS
+
